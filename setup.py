7e5ce45aa5602dce421c2ead33095a11bc54a56b
Randall Shane, PhD <randall.shane@icloud.com>
2019-04-14 10:28:51 -0600

    Whoops!  Actually eliminated depencendy references.  lol

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fkscore",
-    version="1.1.4",
+    version="1.1.5",
 
     description="Flesch Kincaid readability scoring algorithm",
     long_description=long_description,
